38bd5098cfd2bbad225e41add6f364590afe11ae
vignespacap <vignesh.palanisamy@capillarytech.com>
2022-03-30 10:12:56 +0530

    session_6

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -6,11 +6,12 @@ const Content = () => {
   const [selectedItemName, setSelectedItemName] = React.useState();
   const [showSpinner, setShowSpinner] = React.useState(false);
   const [dataList, setDataList] = React.useState([]);
+  const [selectedCategory, setSelectedCategory] = React.useState();
 
   React.useEffect(() => {
-    console.log("USE EFFECT CALLED");
+    console.log("COMPONENT MOUNT");
     setShowSpinner(true);
-    
+
     fetch("https://api.publicapis.org/entries")
       .then((res) => res.json())
       .then((data) => {
@@ -19,12 +20,21 @@ const Content = () => {
       });
   }, []);
 
+  React.useEffect(() => {
+    console.log("COMPONENT UPDATE");
+    const selectedItem = dataList.find(({ API }) => selectedItemName === API);
+    console.log({ selectedItem });
+    const { Category } = selectedItem || {};
+    setSelectedCategory(Category);
+  }, [selectedItemName]);
+
   const handleClick = (selectedCardName) => {
     setSelectedItemName(selectedCardName);
   };
 
   return (
     <div className="container">
+      <p>Selcted Category : {selectedCategory || "-"}</p>
       {showSpinner ? (
         <h1>Loading !!!</h1>
       ) : (
